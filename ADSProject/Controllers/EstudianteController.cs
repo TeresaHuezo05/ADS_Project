d14e6aec095081fce333724e7fc82536a18a7bf0
TERESA GUADALUPE HUEZO SALAVERRIA <hs19i04002@usonsonate.edu.sv>
2022-05-29 13:33:52 -0600

    cambio

--- ADSProject/Controllers/EstudianteController.cs
+++ ADSProject/Controllers/EstudianteController.cs
@@ -12,10 +12,12 @@ namespace ADSProject.Controllers
     public class EstudianteController : Controller
     {
         private readonly IEstudianteRepository estudianteRepository;
+        private readonly ICarreraRepository carreraRepository;
 
-        public EstudianteController(IEstudianteRepository estudianteRepository)
+        public EstudianteController(IEstudianteRepository estudianteRepository, ICarreraRepository carreraRepository)
         {
             this.estudianteRepository = estudianteRepository;
+            this.carreraRepository = carreraRepository;
         }
 
         [HttpGet]
@@ -47,6 +49,8 @@ namespace ADSProject.Controllers
                 }
                 // Indica el tipo de operacion que es esta realizando
                 ViewData["Operaciones"] = operaciones;
+
+                // 
 
                 return View(estudiante);
 
